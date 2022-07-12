16d520d9b1b19261726a9d9f5494b938ed8ab93f
Edmund Lee <likso@users.noreply.github.com>
2022-07-12 17:06:08 +0800

    Various Fixes and Enhancements
    1. Added async support
    2. Fix entity_id format
    3. Added unique_id property
    4. Added switch SL_NATURE, SL_SW_NS[1|2|3] support
    5. Added light SL_SPOT colour and brightness support
    6. Added light strip SL_CT_RGBW colour and brightness support
    7. Added light SL_LI_WW colour temperature and brightness support
    8. Fixed Door sensor SL_SC_BG open/closed status
    9. Changed server url to https://api.us.ilifesmart.com/
    10. Added excluded tgt setting in configuration.yaml

--- lifesmart/binary_sensor.py
+++ lifesmart/binary_sensor.py
@@ -10,41 +10,66 @@ from . import LifeSmartDevice
 _LOGGER = logging.getLogger(__name__)
 
 
-GUARD_SENSOR = ["SL_SC_G",
-"SL_SC_BG"]
-MOTION_SENSOR = ["SL_SC_MHW",
-"SL_SC_BM",
-"SL_SC_CM"]
+GUARD_SENSOR = ["SL_SC_G", "SL_SC_BG"]
+MOTION_SENSOR = ["SL_SC_MHW", "SL_SC_BM", "SL_SC_CM"]
 SMOKE_SENSOR = ["SL_P_A"]
-def setup_platform(hass, config, add_entities, discovery_info=None):
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Perform the setup for lifesmart devices."""
     if discovery_info is None:
         return
     dev = discovery_info.get("dev")
     param = discovery_info.get("param")
     devices = []
-    for idx in dev['data']:
-        if idx in ["M","G","B","AXS","P1"]:
-            devices.append(LifeSmartBinarySensor(dev,idx,dev['data'][idx],param))
-    add_entities(devices)
+    for idx in dev["data"]:
+        if idx in ["M", "G", "B", "AXS", "P1"]:
+            devices.append(LifeSmartBinarySensor(dev, idx, dev["data"][idx], param))
+    async_add_entities(devices)
+
 
 class LifeSmartBinarySensor(LifeSmartDevice, BinarySensorEntity):
     """Representation of LifeSmartBinarySensor."""
 
     def __init__(self, dev, idx, val, param):
         super().__init__(dev, idx, val, param)
-        self.entity_id = ENTITY_ID_FORMAT.format(( dev['devtype'] + "_" + dev['agt'] + "_" + dev['me'] + "_" + idx).lower())
-        devtype = dev['devtype']
+        self.entity_id = ENTITY_ID_FORMAT.format(
+            (
+                dev["devtype"] + "_" + dev["agt"][:-3] + "_" + dev["me"] + "_" + idx
+            ).lower()
+        )
+        devtype = dev["devtype"]
         if devtype in GUARD_SENSOR:
-            self._device_class = "door"
+            if idx in ["G"]:
+                self._device_class = "door"
+                if val["val"] == 0:
+                    self._state = True
+                else:
+                    self._state = False
+            if idx in ["AXS"]:
+                self._device_class = "vibration"
+                if val["val"] == 0:
+                    self._state = False
+                else:
+                    self._state = True
+            if idx in ["B"]:
+                self._device_class = None
+                if val["val"] == 0:
+                    self._state = False
+                else:
+                    self._state = True
         elif devtype in MOTION_SENSOR:
             self._device_class = "motion"
+            if val["val"] == 0:
+                self._state = False
+            else:
+                self._state = True
         else:
             self._device_class = "smoke"
-        if (val['val'] == 1 and self._device_class != "door") or (val['val'] == 0 and self._device_class == "door"):
-            self._state = True
-        else:
-            self._state = False
+            if val["val"] == 0:
+                self._state = False
+            else:
+                self._state = True
 
     @property
     def is_on(self):
@@ -56,5 +81,7 @@ class LifeSmartBinarySensor(LifeSmartDevice, BinarySensorEntity):
         """Return the class of binary sensor."""
         return self._device_class
 
-
-
+    @property
+    def unique_id(self):
+        """A unique identifier for this entity."""
+        return self.entity_id
